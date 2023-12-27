48005ca15b99948ad4ab38695ea2f1bb31f98a3f
Anna Nowak <annov723@gmail.com>
2023-12-27 17:16:29 +0100

    function declarations

---
+++ quiz-game/src/Quiz.java
@@ -0,0 +1,28 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Quiz implements ActionListener{
+	
+	public Quiz() {
+		
+	}
+	
+	public void nextQuestion() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void displayAnswer() {
+		
+	}
+	
+	public void results() {
+		
+	}
+}
